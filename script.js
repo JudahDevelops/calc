581fc6ec8974aa2424f439e36a3769859dc91da8
JudahDevelops <judahdevelops@gmail.com>
2024-12-13 14:39:00 +0100

    we try again

---
+++ script.js
@@ -0,0 +1,20 @@
+const display = document.getElementById('screen');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.id === '=') {
+            display.innerText = eval(display.innerText);
+        } else if(button.id === 'AC') {
+            display.innerText = '';
+        } else if (button.id === '=' && typeof eval(display.innerText) === 'null') {
+            display.innerText = 'error'
+        } else {
+            display.innerText += button.id;
+        }
+        console.log(typeof eval(display.innerText));
+    });
+})
+
+
+
